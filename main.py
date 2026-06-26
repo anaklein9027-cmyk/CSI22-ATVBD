ef5d89f1860095bc5fc8a9cd5c9a3c72834125d8
anaklein9027-cmyk <ana.klein.9027@ga.ita.br>
2026-06-26 16:15:29 -0300

    Configure tkinter interface in main.py

--- main.py
+++ main.py
@@ -1,2 +1,28 @@
-#aqui na main vou configrar a interface do tkinter
+#aqui na main vou configurar a interface do tkinter
 
+
+class Application:
+    def __init__(self, master=None):
+        self.fontePadrao = ("Arial", "20")
+        self.primeiroContainer = Frame(master)
+        self.primeiroContainer["pady"] = 10
+        self.primeiroContainer.pack()
+
+        self.segundoContainer = Frame(master)
+        self.segundoContainer["pady"] = 20
+        self.segundoContainer.pack()
+
+        self.terceiroContainer = Frame(master)
+        self.terceiroContainer["pady"] = 20
+        self.terceiroContainer.pack()
+
+        self.quartoContainer = Frame(master)
+        self.quartoContainer["pady"] = 20
+        self.quartoContainer.pack()
+
+        self.quintoContainer = Frame(master)
+        self.quintoContainer["pady"] = 20
+        self.quintoContainer.pack()
+root = Tk()
+Application(root)
+root.mainloop()
