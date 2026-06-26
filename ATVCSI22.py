69fc9fed2fa5a30d5b528c3c088bb2ba6b8ab468
anaklein9027-cmyk <ana.klein.9027@ga.ita.br>
2026-06-26 13:18:37 -0300

    Add Prestador class with CRUD methods

--- ATVCSI22.py
+++ ATVCSI22.py
@@ -1,4 +1,22 @@
 #Catálogo de Prestadores de Serviço em Tecnologia da Informação (TI)
+from Banco import Banco #ainda não fiz a parte do banco de dados
 
 class Prestador(object):
-    
+        def __init__(self, ID=0, nome="", CPF_CNPJ="", nascimento="", endereço="", contato=""):
+        self.info = {}
+        self.ID = ID
+        self.nome = nome
+        self.CPF_CNPJ = CPF_CNPJ
+        self.nascimento = nascimento
+        self.endereço = endereço
+        self.contato = contato
+
+        def createPrestador(self):
+           banco = Banco();
+        def readPrestador(self):
+           banco = Banco();
+        def updatePrestador(self):
+           banco = Banco();
+        def deletePrestador(self):
+           banco = Banco();
+           
