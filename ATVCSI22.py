08382801469e57cdcdcb01ee22fb3b6f5c944602
anaklein9027-cmyk <ana.klein.9027@ga.ita.br>
2026-06-26 14:07:14 -0300

    Enhance prestador methods with error handling
    Refactor createPrestador and updatePrestador methods to include error handling and improve code structure.

--- ATVCSI22.py
+++ ATVCSI22.py
@@ -15,11 +15,43 @@ class Prestador(object):
         self.contato = contato
 
         def createPrestador(self):
-           banco = Banco();
+                banco = Banco();
+                try:
+
+                    c = banco.conexao.cursor()
+        
+                    c.execute("insert into prestadores (ID, nome, CPF_CNPJ, nascimento, endereço, contato) values('" + self.ID + "', '" +
+                              self.nome + "', '" + self.CPF_CNPJ + "','" + self.nascimento + "','" + self.endereço + "', '" + self.contato + "' )")
+        
+                    banco.conexao.commit()
+                    c.close()
+        
+                    return "Prestador cadastrado com sucesso!"
+                except:
+                    return "Ocorreu um erro no cadastro do prestador"
+
+        
         def readPrestador(self):
            banco = Banco();
+                
         def updatePrestador(self):
-           banco = Banco();
+                banco = Banco();
+                try:
+        
+                    c = banco.conexao.cursor()
+        
+                    c.execute("update prestadores set nome = '" + self.nome +
+                              "', CPF_CNPJ = '" + self.CPF_CNPJ + "', nascimento = '" + self.nascimento + "', endereço = '" + self.endereço + "', contato = '" + self.contato + "' where ID = " + self.ID + " ")
+                         #não sei se devo fazer a alterção ser por uma busca de ID Ou de nome - talvez nome faça mais sentido
+                    banco.conexao.commit()
+                    c.close()
+        
+                    return "Prestador atualizado com sucesso!"
+                except:  
+                    return "Ocorreu um erro na alteração das informações do prestador"
+
+
+        
         def deletePrestador(self):
            banco = Banco();
            
