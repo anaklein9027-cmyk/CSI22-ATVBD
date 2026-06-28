ad04c3a7bc6a108e2f908c03709420fe861fb8d1
yrbms <yurimendes729@gmail.com>
2026-06-28 19:27:00 -0300

    Create Usuarios.py

---
+++ Usuarios.py
@@ -0,0 +1,63 @@
+from Banco import Banco #ainda não terminei a parte do banco do endereço - acho que a parte do CEP vai precisar de outra função aqui
+from tkinter import *
+from tkinter import ttk
+
+class Usuarios(object):
+    def __init__(self, idusuario = 0, nome = "", telefone = 0, email = "", usuario = "", senha = ""):
+        self.idusuario = idusuario
+        self.nome = nome
+        self.telefone = telefone
+        self.email = email
+        self.usuario = usuario
+        self.senha = senha
+
+    def insertUser(self):
+            banco = Banco();
+            try:
+
+                c = banco.conexao.cursor()
+    
+                c.execute("insert into prestadores (ID, nome, CPF_CNPJ, nascimento, endereço, contato) values('" + str(self.idusuario) + "', '" +
+                            self.nome + "', '" + self.telefone + "', '" + self.email + "','" + self.usuario + "','" + self.senha + "')")
+                
+                #c.execute(f"insert into prestadores (ID, nome, CPF_CNPJ, nascimento, endereço, contato) values({self.idusuario},{self.nome},{self.telefone},{self.email},{self.usuario},{self.senha})")
+    
+                banco.conexao.commit()
+                c.close()
+    
+                return "Prestador cadastrado com sucesso!"
+            except:
+                raise
+                return "Ocorreu um erro no cadastro do prestador"
+            
+    def updateUser(self):
+            banco = Banco();
+            try:
+    
+                c = banco.conexao.cursor()
+    
+                c.execute("update prestadores set nome = '" + self.nome +
+                            "', CPF_CNPJ = '" + self.telefone + "', nascimento = '" + self.email + "', endereço = '" + self.usuario + "', contato = '" + self.senha + "' where ID = " + str(self.idusuario) + " ")
+                        #não sei se devo fazer a alterção ser por uma busca de ID Ou de nome - talvez nome faça mais sentido
+                banco.conexao.commit()
+                c.close()
+    
+                return "Prestador atualizado com sucesso!"
+            except:
+                raise
+                return "Ocorreu um erro na alteração das informações do prestador"
+            
+    def deleteUser(self):
+            banco = Banco();
+            try:
+
+                c = banco.conexao.cursor()
+    
+                c.execute("delete from prestadores where ID = " + str(self.idusuario) + " ")
+    
+                banco.conexao.commit()
+                c.close()
+
+                return "Prestador excluido com sucesso!"
+            except:
+                return "Ocorreu um erro na exclusão do prestador"
